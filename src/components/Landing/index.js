7b7ed7fb4265322f1454a465069a04bd01ccb1c3
titusjin <titus@deepblu.com>
2017-12-12 19:08:00 +0800

    updated landing page

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -21,25 +21,25 @@ const contextTypes = {
 class Landing extends React.Component {
   constructor (props) {
     super(props)
+
+    this.state = {
+      checkedOption = ''
+    }
+
+    this.handleUserLanding = this.handleUserLanding.bind( this )
   }
 
-  handleBusinessLink (e) {
-    e.preventDefault()
-    // this.context.router.push('/form/businessRegist')
-  }
-  handleDiverLink (e) {
-    e.preventDefault()
-    // this.context.router.push('/form/diverRegist')
+  handleUserLanding (e) {
+    console.log( e.target.value )
+    if( e.target.checked ){
+      console.log('hello')
+      !e.target.checked
+    }else{
+      e.targe.checked = true;
+    }
   }
 
   render () {
-    // const diverStyle = {
-    //   backgroundImage: `url(${diver})`
-    // }
-    // const businessStyle = {
-    //   backgroundImage: `url(${business})`
-    // }
-
     return (
       <div className={cx('container')}>
         <div className={cx('title')}>
@@ -51,11 +51,11 @@ class Landing extends React.Component {
           </div>
           <div className={cx('content-input')}>
             <div className={cx('content-item')}>
-              <input type="radio" name="land-choose" value="aud" checked={true}/>
+              <input type="radio" name="land-choose" value="aud" checked={true} onChange={ this.handleUserLanding }/>
               <label>Choose One Event</label>
             </div>
             <div className={cx('content-item')}>
-              <input type="radio" name="land-choose" value="admin"/>
+              <input type="radio" name="land-choose" value="admin" onChange={ this.handleUserLanding }/>
               <label>Login As Admin</label>
             </div>
           </div>
